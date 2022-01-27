606b34e906b330c1054ca0dac32f11650a7b614f
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-01-27 11:59:22 +0900

    - Desactivation fonction sur la majoritée - Commantaires et mise en forme

--- modal.js
+++ modal.js
@@ -14,22 +14,21 @@ const formData = document.querySelectorAll(".formData");
 const form = document.querySelector('form');
 const modalClose = document.querySelector(".close") //Création et chargement de la constante 'modalClose' et accès à la class .close du DOM
 const modalSubmitClose = document.getElementById("submitClose")
+const body = document.querySelector('body');
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
-//Stockage des valeurs de champs dans des variables//
+//Stockage des valeurs de champs dans des variables
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const birthdateInput = document.getElementById("birthdate");
 const birthdateError = document.getElementById("birthdateError")
 const quantityTournois = document.getElementById("quantity");
 const quantityError = document.getElementById("quantityError")
-//Controle du format du mail//
+const cgv = document.getElementById("checkbox1");
+//Controle du format du mail
 const emailAdress = document.getElementById("email");
 const mailError = document.getElementById("mailError");
 const mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
-const cgv = document.getElementById("checkbox1");
-const body = document.querySelector('body')
-
 
 // launch modal form
 function launchModal() {
@@ -52,16 +51,16 @@ function close() {
 }
 
 const clearInput = () => {
-//Suppression de ce qui a pu etre entré
- form.reset();
- 
-//Suppression des cadres rouges
+  //Suppression de ce qui a pu etre entré
+  form.reset();
+
+  //Suppression des cadres rouges
   const resetErrorBorder = document.querySelectorAll('input')
   resetErrorBorder.forEach((element) => {
-   element.classList.remove ('errorForm');
-});
+    element.classList.remove('errorForm');
+  });
 
-//Suppression des messages d'erreur
+  //Suppression des messages d'erreur
   const resetErrorMessage = document.querySelectorAll('.result')
   resetErrorMessage.forEach((message) => {
     message.innerHTML = '';
@@ -83,18 +82,18 @@ function prenomValidation() {
   //Chaque fois que l'utilisateur saisit quelque chose
   //On vérifie la validité du champ prénom
   //Si rien n'est écrit ou s'il n'y a que des espaces
-  if (firstName.value.trim().length == 0){
+  if (firstName.value.trim().length == 0) {
     firstError.innerHTML = 'Réponse obligatoire!'; //ajout du message
     firstError.classList.add('errorStyle'); //ajout d'une class au message d'erreur
     first.classList.add('errorForm'); //ajout d'une class a la zone d'entrée
     return false;
-//s'il y a moins de 2 caracteres ou que des espaces
+    //s'il y a moins de 2 caracteres ou que des espaces
   } else if (firstName.value.trim().length < 2) {
-    firstError.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.'; 
+    firstError.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
     firstError.classList.add('errorStyle');
     first.classList.add('errorForm');
     return false;
-//si toutes les conditions sont remplies
+    //si toutes les conditions sont remplies
   } else {
     firstError.innerHTML = ' ';
     return true;
@@ -109,7 +108,7 @@ function nomValidation() {
   last.classList.remove('errorForm')
   // Chaque fois que l'utilisateur saisit quelque chose
   // on vérifie la validité du champ
-  //si rien n'est écrit ou s'il n'y a que des espaces
+  //si rien n'est écrit ou s'il n'y a que des espaces (.trim())
   if (lastName.value.trim().length == 0) {
     lastError.innerHTML = 'Réponse obligatoire!'; //ajout du message
     lastError.classList.add('errorStyle') //ajout d'une class au message d'erreur
@@ -135,20 +134,20 @@ function mailValidation() {
   mailError.classList.remove('errorStyle');
   email.classList.remove('errorForm');
 
-//Si rien n'a été indiqué
+  //Si rien n'a été indiqué
   if (emailAdress.value == "") {
     mailError.innerHTML = 'Entrez une adresse mail.';
     mailError.classList.add('errorStyle');
     email.classList.add('errorForm');
     return false;
-//Si la valeur du mail ne correspond pas au standard
+    //Si la valeur du mail ne correspond pas au standard
   } else if (!emailAdress.value.match(mailRegex)) {
     mailError.innerHTML = 'Adresse mail invalide.';
     mailError.classList.add('errorStyle')
     email.classList.add('errorForm')
     return false;
   }
-  
+
   else {
     mailError.innerHTML = '';
     return true;
@@ -157,24 +156,29 @@ function mailValidation() {
 
 //Controle age
 function birthdateValidation() {
-  var majorite = -567648000000;
-  if (birthdateInput.value == ""){
+  //var majorite = -567648000000;
+  if (birthdateInput.value == "") {
     birthdateError.innerHTML = 'Vous devez entrer votre date de naissance.';
     birthdateError.classList.add('errorStyle')
     birthdate.classList.add('errorForm')
     return false;
 
-  } else if (Date.parse(birthdateInput.value) > Date.now()){
+  } else if (Date.parse(birthdateInput.value) > Date.now()) {
     birthdateError.innerHTML = 'Vous devez entrer une date valide.';
     birthdateError.classList.add('errorStyle')
     birthdate.classList.add('errorForm')
     return false;
 
+    //Fonction qui calcule la majoritée.
+    //Elle ne prend pas en compte les années bissextiles,
+    //ce qui fausse le resultat de quelques jours
+    /*
   } else if ((Date.parse(birthdateInput.value)-Date.now()) > majorite) {
     birthdateError.innerHTML = "Vous n'avez pas la majorité pour vous inscrire";
     birthdateError.classList.add('errorStyle')
     birthdate.classList.add('errorForm')
     return false;
+    */
 
   } else {
     birthdateError.innerHTML = '';
@@ -185,7 +189,8 @@ function birthdateValidation() {
 };
 
 //Control nombre de tournois
-function checkNb() { 
+function checkNb() {
+  //Charge la valeur
   var nbTournois = document.getElementById("quantity").value;
   //Si ce n'est pas un chiffre
   if (isNaN(nbTournois)) {
@@ -193,13 +198,13 @@ function checkNb() {
     quantityError.classList.add('errorStyle')
     quantity.classList.add('errorForm');
     return false;
-  //Si rien n'est indiqué
-  } else if (nbTournois=="") {
+    //Si rien n'est indiqué
+  } else if (nbTournois == "") {
     quantityError.innerHTML = "Merci d'indiquer le nombre de participations.";
     quantityError.classList.add('errorStyle')
     quantity.classList.add('errorForm');
     return false;
-//Sinon
+    //Sinon
   } else {
     quantityError.innerHTML = '';
     quantityError.classList.remove('errorStyle')
@@ -210,30 +215,32 @@ function checkNb() {
 
 //Selection d'une ville
 function locationValidation() {
-//initialise les variables
-var valid = false;
-var x = document.reserve.location;
+  //initialise les variables
+  var valid = false;
+  var x = document.reserve.location;
 
-//on crée une boucle qui regarde si un élément est "checked"
-for(var i=0; i<x.length; i++) {
-  if(x[i].checked){
-    valid = true;
-    //On termine la boucle quand un element est selectionné 
-    break;
+  //on crée une boucle qui regarde si un élément est "checked"
+  for (var i = 0; i < x.length; i++) {
+    if (x[i].checked) {
+      valid = true;
+      //On termine la boucle quand un element est selectionné,
+      //C'est a dire "true"
+      break;
+    }
+  }
+  //On retourne la valeur en fonction de la variable 'valide' ou non
+  if (valid) {
+    locationError.innerHTML = ' ';
+    locationError.classList.remove('errorStyle')
+    return true;
+  } else {
+    locationError.innerHTML = 'Vous devez choisir une option.';
+    locationError.classList.add('errorStyle')
+    return false;
   }
 }
-//On retourne la valeur en fonction de la variable 'valide'
-if(valid){
-  locationError.innerHTML = ' ';
-  locationError.classList.remove('errorStyle')
-  return true;
-} else {
-  locationError.innerHTML = 'Vous devez choisir une option.';
-  locationError.classList.add('errorStyle')
-  return false;
-}
-}
 
+//Simple verification du statut "checked"
 function checkCGV() {
   if (cgv.checked) {
     cgvError.innerHTML = ' ';
@@ -246,7 +253,7 @@ function checkCGV() {
   }
 };
 
-//Ecoute des evenements dans chaque input et la fonction associée est lancée 
+//Ecoute des evenements dans chaque input et lance la fonction associée
 //Prénom
 firstName.addEventListener('input', prenomValidation);
 //Nom
@@ -267,9 +274,9 @@ document
 
 //Envoi du formulaire
 document.getElementById("registrationForm").onsubmit = function (event) {
-//on n'execute pas l'action par defaut
+  //on n'execute pas l'action par defaut
   event.preventDefault();
-//Charge les fonctions ecoutant les inputs
+  //Lance les fonctions et regarde les inputs
   prenomValidation();
   nomValidation();
   mailValidation();
@@ -278,7 +285,8 @@ document.getElementById("registrationForm").onsubmit = function (event) {
   locationValidation();
   checkCGV();
 
-//Verifie si toutes les conditions sont remplies
+  //Verifie si toutes les conditions sont remplies
+  //C'est a dire "true"
   if (
     prenomValidation() == true &&
     nomValidation() == true &&
@@ -287,8 +295,7 @@ document.getElementById("registrationForm").onsubmit = function (event) {
     checkNb() == true &&
     locationValidation() == true &&
     checkCGV() == true
-    )
-  {
+  ) {
     //Si tout est bon, on enleve le formulaire et le mesage de validation s'affiche
     document.querySelector('.modal-body').style.display = 'none';
     document.querySelector('.message-validation').style.display = 'flex';
